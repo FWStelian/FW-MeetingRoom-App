447c021fbdf65714cfdd2f88877e86b3842fb5db
stelian <stelian@futureworkshops.com>
2019-02-22 14:39:06 +0000

    define theme stub

---
+++ lib/ui/theme/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const someBlack = Color(0xFF202A25);
+const kindOfCyan = Color(0xFF9eFFF3);
+const someWeirdGray = Color(0xFF8DA2B2);
+const poopyYellow = Color(0xFFE8C547);
+const dilutedBloodRed = Color(0xFFC97064);
