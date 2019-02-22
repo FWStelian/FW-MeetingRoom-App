54a4c244b2783bfd64b88beaf982158d62ad0901
stelian <stelian@futureworkshops.com>
2019-02-22 14:38:48 +0000

    add http dependency and mock api call for room meetings

--- lib/data/fw_meetings_provider.dart
+++ lib/data/fw_meetings_provider.dart
@@ -1,18 +1,21 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:fw_meetings/models/booking_model.dart';
 import 'package:fw_meetings/models/room_model.dart';
 
 class FwMeetingsProvider {
   Future<List<BookingModel>> getMeetingsForRoom(String roomName) async {
-//    final response = await client.request(endpoint);
-    return _buildBookingModelList(Iterable.empty());
-  }
+    final response =
+    await http.get('https://jsonplaceholder.typicode.com/posts/1');
 
-//  Future<BookingModel> getMeetingsForRoom(Endpoint endpoint, String roomName) async {
-//    final apiKey = await kNewsApiKey();
-//    final client = NewsapiClient(apiKey);
-//    final response = await client.request(endpoint);
-//    return ArticleCollectionModel.fromJson(response);
-//  }
+    if (response.statusCode == 200) {
+      return _buildBookingModelList(json.decode(response.body));
+    } else {
+      // If that call was not successful, throw an error.
+      throw Exception('Failed to load booked meetings');
+    }
+  }
 
   static List<BookingModel> _buildBookingModelList(List meetings) {
     List<BookingModel> temp = [];
